3d4724508fc31c0fd441b9b1c2398a0bf81647e6
Bryan Joseph <bryanllana14@gmail.com>
2024-03-08 07:10:37 -0500

    Tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Form from './components/Form'
 import Header from './components/Header'
+import ListPatients from './components/ListPatients'
 
 const App = () => {
   return (
     <>
       <Header /> 
       <Form />
+      <ListPatients />
     </>
   )
 }
